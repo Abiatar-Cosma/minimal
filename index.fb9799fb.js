1423662b953420bfbe73a56d60e596f7b108e9fb
Abiatar-Cosma <156704870+Abiatar-Cosma@users.noreply.github.com>
2024-09-01 14:26:09 +0000

    Deploying to gh-pages from @ Abiatar-Cosma/minimal-team-project@c3c8587a457e9b6beefb3ad286b6b3e83803fe72 🚀

---
+++ index.fb9799fb.js
@@ -0,0 +1,2 @@
+(()=>{let e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),menu:document.querySelector("[data-menu]")};function n(){e.menu.classList.toggle("is-open")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)})();
+//# sourceMappingURL=index.fb9799fb.js.map
